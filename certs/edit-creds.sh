c229c031f21bd1bf7160f09dcd2da12b017682c0
Paul Butler <butler.paul@gmail.com>
2022-06-14 08:48:53 -0500

    scripts for editing credentials
    Signed-off-by: Paul Butler <butler.paul@gmail.com>

--- certs/edit-creds.sh
+++ certs/edit-creds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-source $HOME/certs/config-creds.sh 
-cd $HOME/AVH_Demo/amazon-freertos/demos/include
+./gen-creds.sh
+source secrets.txt 
+cd ../amazon-freertos/demos/include
 envsubst <aws_clientcredential.h.in >aws_clientcredential.h
 envsubst <aws_clientcredential_keys.h.in >aws_clientcredential_keys.h
